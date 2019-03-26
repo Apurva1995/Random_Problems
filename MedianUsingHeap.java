86b8ef8356d626dbfd688bc266a8438863c7428b
Apurva1995 <42551079+Apurva1995@users.noreply.github.com>
2019-03-26 13:15:25 +0530

    Updated correct code

--- MedianUsingHeap.java
+++ MedianUsingHeap.java
@@ -10,24 +10,25 @@ class GFG
 	    
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	    int n, val;
-	    n = Integer.parseInt(br.readLine());
+	    n = Integer.parseInt((br.readLine()).trim());
 	    for(int i=0;i<n;i++) {
-	        val = Integer.parseInt(br.readLine());
+	        val = Integer.parseInt((br.readLine()).trim());
+	        if(minHeap.size() == 0) {
+	            minHeap.add(val);
+	            System.out.println(minHeap.peek());
+	            continue;
+	        }
 	        if(minHeap.size() > 0) {
 	            if(minHeap.peek() < val)
 	                minHeap.add(val);
+	            else
+	                maxHeap.add(val);
 	        }
-	        else if(minHeap.size() == 0)
-	            minHeap.add(val);
-	        else
-	            maxHeap.add(val);
 	            
-	        if(maxHeap.size() > minHeap.size() && (maxHeap.size()-minHeap.size()) > 1) {
+	        if(maxHeap.size() > minHeap.size() && (maxHeap.size()-minHeap.size()) > 1)
 	            minHeap.add(maxHeap.poll());
-	        }
-	        else if(maxHeap.size() < minHeap.size() && (minHeap.size()-maxHeap.size()) > 1) {
+	        else if(maxHeap.size() < minHeap.size() && (minHeap.size()-maxHeap.size()) > 1)
 	            maxHeap.add(minHeap.poll());
-	        }
 	        
 	        if((i+1)%2 == 1) {
 	            if(maxHeap.size()>minHeap.size())
